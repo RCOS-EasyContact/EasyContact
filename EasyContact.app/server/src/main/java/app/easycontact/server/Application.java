4df20f4a5e33c3eb39b922c1d7073da4dab9781d
root <79224316+Rexezuge@users.noreply.github.com>
2022-09-29 16:39:18 +0000

    FEAT: Rename Package to `app.easycontact.server`

--- EasyContact.app/server/src/main/java/app/easycontact/server/Application.java
+++ EasyContact.app/server/src/main/java/app/easycontact/server/Application.java
@@ -1,4 +1,4 @@
-package app.EasyContact.server;
+package app.easycontact.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
