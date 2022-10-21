c59885b3ccb3b5bb4d366a6828ae7859246ae2c9
qit2 <55993238+qit2@users.noreply.github.com>
2022-10-21 16:53:28 -0400

    FEAT: Add Frontend function in the inbox.js (#16)
    Co-authored-by: Reddy017 <95815144+Reddy017@users.noreply.github.com>
    Co-authored-by: Jensen Chen <71129832+ZHIHANCHEN03@users.noreply.github.com>
    Co-authored-by: Reddy017 <1319757050@qq.com>
    Co-authored-by: katyushasama <aoyamamegumi811@gmail.com>
    Co-authored-by: wangz44 <82012966+wangz44@users.noreply.github.com>
    Co-authored-by: xiaokeai55 <934067548@qq.com>

--- EasyContact.app/client/src/components/Inbox.js
+++ EasyContact.app/client/src/components/Inbox.js
@@ -10,6 +10,8 @@ export class Inbox extends Component {
     this.markRead = this.markRead.bind(this);
     this.doShow = this.doShow.bind(this);
     this.doDelete = this.doDelete.bind(this);
+    this.doRecover = this.doRecover.bind(this);
+    this.permaDelete = this.permaDelete.bind(this);
     this.toggleMark = this.toggleMark.bind(this);
     this.toggleMarkAll = this.toggleMarkAll.bind(this);
     this.deleteMarked = this.deleteMarked.bind(this);
@@ -27,39 +29,97 @@ export class Inbox extends Component {
 
   markRead(idx) {
     /* mark this message as read */
-
+    let messages = [...this.state.messages];
+    messages[idx].read = true;
+    this.setState({ messages });
   }
 
   doShow(idx) {
-  
+    this.markRead(idx);
+    this.setState({
+      selected: messages[idx]
+    });
+    /* open message in modal */
+    this.ModalMessage.current.show();
   }
 
   doCompose() {
-   
+   /* open compose modal */
+   this.ModalCompose.current.show();
   }
 
   toggleMark(idx) {
-   
+    let messages = [...this.state.messages];
+    messages[idx].marked = messages[idx].marked ? 0 : 1;
+    this.setState({ messages });
   }
 
   doDelete(idx) {
-    
+    let messages = [...this.state.messages];
+    let deleted = [...this.state.deleted];
+    /* append it to deleted */
+    deleted.push(messages[idx]);
+    /* remove the message at idx */
+    messages.splice(idx, 1);
+    this.setState({ messages, deleted });
   }
 
   toggleMarkAll() {
-   
+    let messages = [...this.state.messages];
+    messages.map((v, k) => {
+      return (v.marked = v.marked ? 0 : 1);
+    });
+    this.setState({ messages });
   }
 
   deleteMarked() {
-    
+    let messages = [...this.state.messages];
+    let deleted = [...this.state.deleted];
+    let temp_store_message_idx = [];
+    for(let i = 0; i < messages.length;i++){
+      //console.log(messages[i]);
+      if(messages[i].marked === undefined){
+        console.log("und");
+      }
+      else if (messages[i].marked === 1){
+        deleted.push(messages[i]);
+        temp_store_message_idx.push(i); 
+      }
+    }
+    for(let temp = temp_store_message_idx.length-1; temp >= 0;temp--){
+      console.log(temp_store_message_idx[temp]);
+      messages.splice(temp_store_message_idx[temp], 1);
+    }
+    this.setState({ messages, deleted });
   }
 
   refreshMessages() {
-   
+    let initMessages = [...this.state.initMessages];
+    let deleted = [...this.state.deleted];
+    deleted = [];
+    this.setState({ messages: initMessages,deleted });
   }
 
   deleteMessages(arr) {
     
+  }
+
+  doRecover(idx) {
+    let messages = [...this.state.messages];
+    let deleted = [...this.state.deleted];
+    /* append it to message */
+    messages.push(deleted[idx]);
+    /* remove the deleted at idx */
+    deleted.splice(idx, 1);
+    this.setState({ messages, deleted });
+  }
+
+  permaDelete(idx) {
+    let messages = [...this.state.messages];
+    let deleted = [...this.state.deleted];
+    /* remove the message at idx */
+    deleted.splice(idx, 1);
+    this.setState({ messages, deleted });
   }
 
   render() {
