d4053c8c1c196959f28a2869452644f448e62725
xiaoshaoyc <xiaoshaoyc@gmail.com>
2022-11-21 22:57:38 -0500

    Import graphql package & Add sample code

--- EasyContact.app/server/src/main/test/app/easycontact/emailsender/SendEmailServiceImplTest.java
+++ EasyContact.app/server/src/main/test/app/easycontact/emailsender/SendEmailServiceImplTest.java
@@ -1,6 +1,14 @@
 package app.easycontact.emailsender;
 
 import app.easycontact.server.Application;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 @SpringBootTest
 @ContextConfiguration(classes = Application.class)
@@ -26,5 +36,26 @@ class SendEmailServiceImplTest {
         String to = env.getRequiredProperty("easycontact.email.to");
         log.info("Sending test email");
         sendEmailService.sendSimpleMessage(from, to, "testtest", "teststsetstetest");
+    }
+
+    private boolean verifyEmail() {
+        String schema = "type Query{hello: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{hello}");
+
+        System.out.println(executionResult.getData().toString());
+        // Prints: {hello=world}
+        return true;
     }
 }
