cc39530efbd13133601fd6097830932282b58bf4
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2022-11-22 19:05:42 -0500

    FEAT: Remove `ExampleMain.java`

--- EasyContact.app/server/src/main/java/app/easycontact/server/imap/ExampleMain.java
+++
@@ -1,8 +0,0 @@
-package app.easycontact.server.imap;
-
-public class ExampleMain {
-    public static void main(String[] args) {
-        EmailReceiver emailReceiver = EmailReceiverImplementation.builder().serverName("mail.rpi.edu").serverPort("993").userName("gongz3").userPassword("DO7gTmQVDeTjHWfU").build();
-        emailReceiver.downloadEmails("INBOX");
-    }
-}
